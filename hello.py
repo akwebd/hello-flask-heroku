2efdc37be65ea64d69864fe58f5b8af21a23114d
akWEBD <akwebd@gmail.com>
2022-02-23 22:12:16 +0200

    One page app only

--- hello.py
+++ hello.py
@@ -2,11 +2,9 @@ from flask import Flask
 import notify
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Index Page'
 
-@app.route('/mail')
+
+@app.route('/')
 def mail():
     notify
     return 'sent'
