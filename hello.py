54d396e6b923ba367bc4038bd2805e24affe1232
akWEBD <akwebd@gmail.com>
2022-03-03 21:10:54 +0200

    Added loop to test 24/7 operation

--- hello.py
+++ hello.py
@@ -1,13 +1,24 @@
 from flask import Flask
 import notify
+from time import sleep
 app = Flask(__name__)
 
 
 
+'''
 @app.route('/')
 def mail():
     notify
     return 'sent'
+'''
+number = 5
+while(True):
+    number=-1
+    notify
+    sleep(60)
+    if(number<0):
+        break
+    
 
 
 if __name__ == '__main__':
